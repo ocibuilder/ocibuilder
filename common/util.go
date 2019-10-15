3bf62f7611cd153b819f78ca8107fc4ecc475780
Art Begolli <artbegolli@gmail.com>
2019-10-15 09:30:31 +0100

    fix(): updating Gopkg.lock and adding dependency in util.go

--- common/util.go
+++ common/util.go
@@ -18,12 +18,13 @@ package common
 
 import (
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // GetClientConfig return rest config, if path not specified, assume in cluster config
 func GetClientConfig(kubeconfig string) (*rest.Config, error) {
-	//if kubeconfig != "" {
-		//return clientcmd.BuildConfigFromFlags("", kubeconfig)
-	//}
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
 	return rest.InClusterConfig()
 }
