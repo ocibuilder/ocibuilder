07ce9616467234934bb13de5ae8b40a3b950a0c7
Art Begolli <artbegolli@gmail.com>
2019-11-20 12:15:40 +0000

    fix(): update logging

---
+++ pkg/oci/builder_test.go
@@ -0,0 +1,22 @@
+package oci
+
+import "testing"
+
+func TestBuilder_Build(t *testing.T) {
+}
+
+func TestBuilder_Pull(t *testing.T) {
+}
+
+func TestBuilder_Push(t *testing.T) {
+}
+
+func TestBuilder_Login(t *testing.T) {
+}
+
+func TestBuilder_Clean(t *testing.T) {
+}
+
+func TestBuilder_Purge(t *testing.T) {
+
+}
