e1b7064b13d6ded951f288a85c229c9770b5c625
Art Begolli <artbegolli@gmail.com>
2019-12-12 11:40:08 +0000

    feat(): adding labels to builder

--- pkg/oci/builder.go
+++ pkg/oci/builder.go
@@ -76,6 +76,7 @@ func (b *Builder) Build(spec v1alpha1.OCIBuilderSpec, res chan<- v1alpha1.OCIBui
 				Dockerfile: opt.Dockerfile,
 				Tags:       []string{imageName},
 				Context:    buildContext,
+				Labels:     opt.Labels,
 			},
 		}
 
