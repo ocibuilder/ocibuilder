eca5cf009054a7bfbedc7c15ba23e5817e753ef5
Art Begolli <artbegolli@gmail.com>
2020-01-07 17:42:37 +0000

    fix(): fix failing test

--- pkg/overlay/overlay.go
+++ pkg/overlay/overlay.go
@@ -25,7 +25,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/docker/docker/api/types"
 	cmdcore "github.com/k14s/ytt/pkg/cmd/core"
 	cmdtpl "github.com/k14s/ytt/pkg/cmd/template"
 	"github.com/k14s/ytt/pkg/files"
